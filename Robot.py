5b53e296fe7dec74762fc9663b2123c336eea45e
James Kistner <36495202+JamesKistner@users.noreply.github.com>
2020-03-09 11:48:14 +1300

    Update Robot.py
    Monday Edits

--- Robot.py
+++ Robot.py
@@ -9,50 +9,78 @@ drive = MoveTank(OUTPUT_B , OUTPUT_C)
 cs = ColorSensor()
 sound = Sound()
 
-def moveForward():
-    drive.on_for_rotations(SpeedPercent(10), SpeedPercent(10), 0.25)
+def moveForward(a = 10, b = 10, c = 0.20):
+    drive.on_for_rotations(SpeedPercent(a), SpeedPercent(b), c)
 
-def turnLeft():
-    drive.on_for_rotations(SpeedPercent(-10), SpeedPercent(10), 0.5)
-def turnRight():
-    drive.on_for_rotations(SpeedPercent(10), SpeedPercent(-10), 0.5)
+def turnLeft(a = -10, b = 10):
+    drive.on_for_rotations(SpeedPercent(a), SpeedPercent(b), 0.5)
+def turnRight(a = 10, b = -10):
+    drive.on_for_rotations(SpeedPercent(a), SpeedPercent(b), 0.5)
+
 def pivotRight():
-    drive.on_for_rotations(SpeedPercent(10), SpeedPercent(0), 0.1)
+    drive.on_for_rotations(SpeedPercent(5), SpeedPercent(0), 0.075)
+def pivotRight90():
+    drive.on_for_rotations(SpeedPercent(10), SpeedPercent(0), 0.5)
+def pivotRightReverse():
+    drive.on_for_rotations(SpeedPercent(-5), SpeedPercent(0), 0.075)
 def pivotLeft():
-    drive.on_for_rotations(SpeedPercent(10), SpeedPercent(0), 0.1)
-def correct():
+    drive.on_for_rotations(SpeedPercent(0), SpeedPercent(5), 0.075)
+def pivotLeftReverse():
+    drive.on_for_rotations(SpeedPercent(0), SpeedPercent(-5), 0.075)
+def check():
     count = 0
     while (cs.value() <= 40):
-        pivotRight()
-        count = count + 1
-    for i in len(count):
         pivotRightReverse()
+        count = count + 1
+        sleep(0.1)
+    for i in range(count):
+        pivotRight()
+        sleep(0.1)
     rightPiv = count
     count = 0
     while (cs.value() <= 40):
-        pivotLeft()
-        count = count + 1
-    for i in len(count):
         pivotLeftReverse()
+        count = count + 1
+        sleep(0.1)
+    for i in range(count):
+        pivotLeft()
+        sleep(0.1)
     leftPiv = count
 
     correctPiv = rightPiv - leftPiv
+    return correctPiv
 
-    if (correctPiv < 0):
-        correctLeft()
-    elif(correctPiv > 0):
-        correctRight()
+def correct(pivotValue):
+    if (pivotValue > 0):
+        move = [10.5, 10]
+    elif (pivotValue < 0):
+        move = [10, 10.5]
+    else:
+        move = [10, 10]
+    return move
 
 cs.mode='COL-REFLECT'
 
-onBlack = False
-blackTiles = 0
+sound.speak(str(5) + "rotations")
+
+onBlack = True
+blackTiles = 1
+sound.beep()
+move = [10, 10]
 while True:
     if (cs.value() <= 40 and onBlack == False):
-        onBlack = True
-        blackTiles = blackTiles + 1
-        sound.beep()
-        correct()
+        if(blackTiles < 3):
+            onBlack = True
+            blackTiles = blackTiles + 1
+            sound.beep()
+        else:
+            onBlack = True
+            moveForward(c = 0.075)
+            blackTiles = blackTiles + 1
+            sound.beep()
+            pivotValue = check()
+            print(pivotValue)
+            move = correct(pivotValue)
     elif (cs.value() <= 40):
         onBlack = True
     else:
@@ -61,14 +89,23 @@ while True:
     if (blackTiles >= 7):
         break
 
-    moveForward()
+    moveForward(move[0], move[1])
+
 sleep(2)
-turn90Right()
-sleep(2)
-moveForward()
-moveForward()
-moveForward()
+sound.beep()
+pivotRight90()
 blackTiles = 0
+
+count = 0
+while blackTiles < 1:
+    if (cs.value() <= 40):
+        blackTiles = blackTiles + 1
+    moveForward()
+    count = count + 1
+
+
+sound.speak(str(count) + "rotations")
+
 #continuous = True
 '''while True:
     if (cs.value() <= 40 and continuous):
@@ -94,5 +131,27 @@ blackTiles = 0
         turnRight()
         rt = True'''
 
+
+
+
+
+
+
+
+
+'''while True:
+    while (cs.value() <= 25):
+        moveForward()
+    while (cs.value() >= 25 and rt == True):
+        turnRight()
+    while (cs.value() >= 25 and rt == False):
+        turn90Left()
+
+    if(rt == True):
+        turnleft()
+        rt = False
+    else:
+        turnRight()
+        rt = True'''
 
 
